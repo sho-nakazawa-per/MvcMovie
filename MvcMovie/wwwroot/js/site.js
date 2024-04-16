a924549562e7da5dc4c061d818b9ec1ea98b8c02
Your Name <you@example.com>
2024-04-16 15:43:00 +0900

    update code

--- MvcMovie/wwwroot/js/site.js
+++ MvcMovie/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+const MOVIEAPP_API = 'https://movieapp-student02.azurewebsites.net/api/movie';
 // Write your JavaScript code.
 
 const $id = (id) => document.getElementById(id);
